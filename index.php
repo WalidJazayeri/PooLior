2d101038c37db2eac8d36fb829453c099c3e7d12
Walid JAZAYERI <jazayeri.walid1@gmail.com>
2024-04-15 09:09:24 +0200

    ajout function getpdo()

--- index.php
+++ index.php
@@ -1,22 +1,10 @@
 <?php
+require_once './libraries/database.php';
 
-/**
- * CE FICHIER A POUR BUT D'AFFICHER LA PAGE D'ACCUEIL !
- *
- * On va donc se connecter à la base de données, récupérer les articles du plus récent au plus ancien (SELECT * FROM articles ORDER BY created_at DESC)
- * puis on va boucler dessus pour afficher chacun d'entre eux
- */
 
-/**
- * 1. Connexion à la base de données avec PDO
- * Attention, on précise ici deux options :
- * - Le mode d'erreur : le mode exception permet à PDO de nous prévenir violament quand on fait une connerie ;-)
- * - Le mode d'exploitation : FETCH_ASSOC veut dire qu'on exploitera les données sous la forme de tableaux associatifs
- */
-$pdo = new PDO('mysql:host=localhost;dbname=u854470974_cours_poo;charset=utf8', 'u854470974_walid', 'O3ph9F8#l', [
-    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
-]);
+//1. Connexion à la base de données avec PDO
+
+$pdo = getPdo();
 
 
 
