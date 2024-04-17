d637014235bab657f7b09088d4e996cd44465ec1
Walid JAZAYERI <jazayeri.walid1@gmail.com>
2024-04-17 08:03:13 +0200

    test de l'autoloader avec rajout d'une fonction pour mettre le premier caractère en minuscule

--- libraries/autoload.php
+++ libraries/autoload.php
@@ -2,6 +2,8 @@
 spl_autoload_register(function($classname)
 {
     $classname = str_replace("\\" , "/", $classname);
+    $classname = lcfirst($classname);
+    var_dump($classname);
     require_once "./libraries/$classname.php";
 })
 ?>
