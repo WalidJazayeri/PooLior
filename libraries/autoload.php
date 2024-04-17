a01bfca9d8ef39ddc1b3298ccf54ec7faa711f1f
Walid JAZAYERI <jazayeri.walid1@gmail.com>
2024-04-17 07:39:46 +0200

    Revert "test"
    This reverts commit bf8413cfce93f1e4e5012a9392ac189c2bf116e9.

--- libraries/autoload.php
+++ libraries/autoload.php
@@ -2,7 +2,6 @@
 spl_autoload_register(function($classname)
 {
     $classname = str_replace("\\" , "/", $classname);
-    var_dump($classname);
     require_once "./libraries/$classname.php";
 })
 ?>
