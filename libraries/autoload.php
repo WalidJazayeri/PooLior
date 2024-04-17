bf8413cfce93f1e4e5012a9392ac189c2bf116e9
Walid JAZAYERI <jazayeri.walid1@gmail.com>
2024-04-17 07:30:20 +0200

    test

--- libraries/autoload.php
+++ libraries/autoload.php
@@ -2,6 +2,7 @@
 spl_autoload_register(function($classname)
 {
     $classname = str_replace("\\" , "/", $classname);
+    var_dump($classname);
     require_once "./libraries/$classname.php";
 })
 ?>
