e4079b7c544384bcb48af1b653791b4f82a2e6be
Walid JAZAYERI <jazayeri.walid1@gmail.com>
2024-04-17 08:06:25 +0200

    var dump retiré dans le fichier autoload.php

--- libraries/autoload.php
+++ libraries/autoload.php
@@ -3,7 +3,6 @@ spl_autoload_register(function($classname)
 {
     $classname = str_replace("\\" , "/", $classname);
     $classname = lcfirst($classname);
-    var_dump($classname);
     require_once "./libraries/$classname.php";
 })
 ?>
