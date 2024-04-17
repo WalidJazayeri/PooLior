12d07b47fec25c83a44f1943eeabdb2ba091c661
Walid JAZAYERI <jazayeri.walid1@gmail.com>
2024-04-17 07:55:01 +0200

    test de l'autoload sur le site hebergé avec un remplacement des séparateurs

--- libraries/autoload.php
+++ libraries/autoload.php
@@ -1,7 +1,9 @@
 <?php
 spl_autoload_register(function($classname)
 {
-    $classname = str_replace("\\" , "/", $classname);
+    var_dump($classname);
+    $classname = str_replace("\\" , DIRECTORY_SEPARATOR, $classname);
+    var_dump($classname);
     require_once "./libraries/$classname.php";
 })
 ?>
